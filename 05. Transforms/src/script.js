6c545a6dc0f08f508086204d43e69b9f9920e6cb
Jose Sentis <jose.sentis1987@gmail.com>
2021-04-20 13:53:25 +0200

    Creates group and adds boxes inside

--- 05. Transforms/src/script.js
+++ 05. Transforms/src/script.js
@@ -10,27 +10,54 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const mesh = new THREE.Mesh(geometry, material)
+// scene.add(mesh)
 
-// Position
-// mesh.position.x = 0.7;
-// mesh.position.y = -0.6;
-// mesh.position.z = 1;
-mesh.position.set(0.7, -0.6, 1)
+// // Position
+// // mesh.position.x = 0.7;
+// // mesh.position.y = -0.6;
+// // mesh.position.z = 1;
+// mesh.position.set(0.7, -0.6, 1)
 
-// Scale
-// mesh.scale.x = 2;
-// mesh.scale.y = 0.5;
-// mesh.scale.z = 0.5;
-mesh.scale.set(2, 0.5, 0.5);
+// // Scale
+// // mesh.scale.x = 2;
+// // mesh.scale.y = 0.5;
+// // mesh.scale.z = 0.5;
+// mesh.scale.set(2, 0.5, 0.5);
 
-// Rotate
-mesh.rotation.reorder('Y');
-mesh.rotation.y = Math.PI * 0.25;
-mesh.rotation.x = Math.PI * 0.25;
+// // Rotate
+// mesh.rotation.reorder('Y');
+// mesh.rotation.y = Math.PI * 0.25;
+// mesh.rotation.x = Math.PI * 0.25;
+
+// Creating cubes group
+const group = new THREE.Group();
+scene.add(group);
+
+const cube1 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+group.add(cube1);
+
+const cube2 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+);
+group.add(cube2);
+cube2.position.x = -2;
+
+const cube3 = new THREE.Mesh(
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial({ color: 0x0000ff })
+);
+group.add(cube3);
+cube3.position.x = 2;
+
+group.position.y = 1;
+group.scale.y = 3;
 
 // Axis Helper
 const axisHelper = new THREE.AxisHelper(3)
@@ -49,8 +76,9 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
-
 scene.add(camera)
+
+// camera.lookAt(mesh.position);
 
 /**
  * Renderer
