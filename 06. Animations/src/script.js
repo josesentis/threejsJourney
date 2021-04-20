4840a315b9d94bd47618fa3f9247228a2177a223
Jose Sentis <jose.sentis1987@gmail.com>
2021-04-20 19:06:00 +0200

    Adds other animation examples

--- 06. Animations/src/script.js
+++ 06. Animations/src/script.js
@@ -41,12 +41,25 @@ const tick = () => {
 
     mesh.rotation.y += 1;
   */
+  // Clock
   const elapsedTime = clock.getElapsedTime();
 
-  mesh.rotation.y = elapsedTime * 2 * Math.PI;
+  // Update position 1
+  // mesh.rotation.y = elapsedTime * 2 * Math.PI;
 
+  // Update position 2
+  // mesh.position.y = Math.sin(elapsedTime);
+  // mesh.position.x = Math.cos(elapsedTime);
+
+  // Update position 3
+  // camera.position.y = Math.sin(elapsedTime);
+  // camera.position.x = Math.cos(elapsedTime);
+  // camera.lookAt(mesh.position);
+
+  // Render
   renderer.render(scene, camera)
 
+  // Next call
   window.requestAnimationFrame(tick);
 };
 
