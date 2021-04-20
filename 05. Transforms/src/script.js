34a0031050ae5b76741cabfa49713fef457d5048
Jose Sentis <jose.sentis1987@gmail.com>
2021-04-20 13:23:13 +0200

    Adds positions to mesh

--- 05. Transforms/src/script.js
+++ 05. Transforms/src/script.js
@@ -15,12 +15,18 @@ const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
+// mesh.position.x = 0.7;
+// mesh.position.y = -0.6;
+// mesh.position.z = 1;
+
+mesh.position.set(0.7, -0.6, 1)
+
 /**
  * Sizes
  */
 const sizes = {
-    width: 800,
-    height: 600
+  width: 800,
+  height: 600
 }
 
 /**
@@ -34,7 +40,7 @@ scene.add(camera)
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+  canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.render(scene, camera)
