54c8285d88387ee71161ab99f9ae1bedcbfbf734
Jose Sentis <jose.sentis1987@gmail.com>
2021-04-21 18:08:41 +0200

    Uses orbit controls

--- 07. Cameras/src/script.js
+++ 07. Cameras/src/script.js
@@ -1,6 +1,6 @@
-import './style.css'
-import * as THREE from 'three'
-
+import './style.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 /**
  * Cursor
@@ -49,6 +49,10 @@ camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
 
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
 // Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas
@@ -63,10 +67,13 @@ const tick = () => {
 
   // Rotate camera on mouse move
   // To understand, use this link https://www.geogebra.org/m/pyPGsGVc
-  camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
-  camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
-  camera.position.y = cursor.y * 3;
-  camera.lookAt(mesh.position);
+  // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
+  // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
+  // camera.position.y = cursor.y * 3;
+  // camera.lookAt(mesh.position);
+
+  // Update controls
+  controls.update();
 
   // Render
   renderer.render(scene, camera)
