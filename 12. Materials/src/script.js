7f285fd0556a6380205ec7fb64843cfa9c1affc6
Jose Sentis <jose.sentis1987@gmail.com>
2021-04-27 13:10:18 +0200

    Using matcaps

--- 12. Materials/src/script.js
+++ 12. Materials/src/script.js
@@ -14,7 +14,7 @@ const heightColorTexture = textureLoader.load('/textures/door/height.jpg');
 const normalColorTexture = textureLoader.load('/textures/door/normal.jpg');
 const metalnessColorTexture = textureLoader.load('/textures/door/metalness.jpg');
 const roughnessColorTexture = textureLoader.load('/textures/door/roughness.jpg');
-const matcapTexture = textureLoader.load('/textures/matcaps/1.png');
+const matcapTexture = textureLoader.load('/textures/matcaps/2.png');
 const gradientTexture = textureLoader.load('/textures/gradients/3.jpg');
 
 /**
@@ -29,14 +29,20 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const material = new THREE.MeshBasicMaterial();
-material.map = doorColorTexture;
+// const material = new THREE.MeshBasicMaterial();
+// material.map = doorColorTexture;
 // material.color = new THREE.Color('yellow');
 // material.wireframe = true;
 // material.opacity = 0.5;
-material.transparent = true;
-material.alphaMap = alphaColorTexture;
-material.side = THREE.DoubleSide;
+// material.transparent = true;
+// material.alphaMap = alphaColorTexture;
+// material.side = THREE.DoubleSide;
+
+// const material = new THREE.MeshNormalMaterial();
+// material.flatShading = true;
+
+const material = new THREE.MeshMatcapMaterial();
+material.matcap = matcapTexture;
 
 const sphere = new THREE.Mesh(
   new THREE.SphereGeometry(0.5, 16, 16),
