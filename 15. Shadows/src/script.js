1d7891e370e5eb2dd81927d7e47e6bff554d582b
Jose Sentis <jose.sentis1987@gmail.com>
2021-05-06 12:05:37 +0200

    Adds helpers to the scene

--- 15. Shadows/src/script.js
+++ 15. Shadows/src/script.js
@@ -26,11 +26,21 @@ scene.add(ambientLight)
 // Directional light
 const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5)
 directionalLight.position.set(2, 2, - 1)
+directionalLight.castShadow = true;
 gui.add(directionalLight, 'intensity').min(0).max(1).step(0.001)
 gui.add(directionalLight.position, 'x').min(- 5).max(5).step(0.001)
 gui.add(directionalLight.position, 'y').min(- 5).max(5).step(0.001)
 gui.add(directionalLight.position, 'z').min(- 5).max(5).step(0.001)
 scene.add(directionalLight)
+
+directionalLight.shadow.height = 1024;
+directionalLight.shadow.width = 1024;
+directionalLight.shadow.camera.near = 1;
+directionalLight.shadow.camera.far = 6;
+
+// Adds helper for the shdows
+const directionalLightCameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera);
+scene.add(directionalLightCameraHelper);
 
 /**
  * Materials
