5b54dd48666f2117a1be26f339141b2b6393a705
Jose Sentis <jose.sentis1987@gmail.com>
2021-05-19 17:28:43 +0200

    Adds doorlight

--- 16. Haunted House/src/script.js
+++ 16. Haunted House/src/script.js
@@ -150,18 +150,23 @@ scene.add(floor)
  * Lights
  */
 // Ambient light
-const ambientLight = new THREE.AmbientLight('#BB91F2', 0.15)
-gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001)
-scene.add(ambientLight)
+const ambientLight = new THREE.AmbientLight('#BB91F2', 0.15);
+gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001);
+scene.add(ambientLight);
 
 // Directional light
-const moonLight = new THREE.DirectionalLight('#BB91F2', 0.15)
-moonLight.position.set(4, 5, - 2)
-gui.add(moonLight, 'intensity').min(0).max(1).step(0.001)
-gui.add(moonLight.position, 'x').min(- 5).max(5).step(0.001)
-gui.add(moonLight.position, 'y').min(- 5).max(5).step(0.001)
-gui.add(moonLight.position, 'z').min(- 5).max(5).step(0.001)
-scene.add(moonLight)
+const moonLight = new THREE.DirectionalLight('#BB91F2', 0.15);
+moonLight.position.set(4, 5, - 2);
+gui.add(moonLight, 'intensity').min(0).max(1).step(0.001);
+gui.add(moonLight.position, 'x').min(- 5).max(5).step(0.001);
+gui.add(moonLight.position, 'y').min(- 5).max(5).step(0.001);
+gui.add(moonLight.position, 'z').min(- 5).max(5).step(0.001);
+scene.add(moonLight);
+
+// Door light
+const doorLight = new THREE.PointLight('#ff7d46', 1, 7);
+doorLight.position.set(0, 2.7, 2.7);
+house.add(doorLight);
 
 /**
  * Sizes
