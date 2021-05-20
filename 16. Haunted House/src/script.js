5cb517e4d35e01af7be4b78a43fee40bbf28a214
Jose Sentis <jose.sentis1987@gmail.com>
2021-05-20 13:49:51 +0200

    Adds door textures

--- 16. Haunted House/src/script.js
+++ 16. Haunted House/src/script.js
@@ -24,6 +24,14 @@ scene.fog = fog;
  */
 const textureLoader = new THREE.TextureLoader();
 
+const doorColorTexture = textureLoader.load('/textures/door/color.jpg');
+const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg');
+const doorAmbientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg');
+const doorHeightTexture = textureLoader.load('/textures/door/height.jpg');
+const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg');
+const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg');
+const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg');
+
 /**
  * House
  */
@@ -49,9 +57,23 @@ house.add(walls);
 
 // Door
 const door = new THREE.Mesh(
-  new THREE.PlaneGeometry(1, 2, 32, 32),
-  new THREE.MeshStandardMaterial({ color: '#aa7b7b' })
+  new THREE.PlaneGeometry(2.2, 2.2, 100, 100),
+  new THREE.MeshStandardMaterial({
+    map: doorColorTexture,
+    alphaMap: doorAlphaTexture,
+    transparent: true,
+    aoMap: doorAmbientOcclusionTexture,
+    displacementMap: doorHeightTexture,
+    displacementScale: 0.1,
+    normalMap: doorNormalTexture,
+    metalnessMap: doorMetalnessTexture,
+    roughnessMap: doorRoughnessTexture
+  })
 )
+door.geometry.setAttribute(
+  'uv2',
+  new THREE.Float32BufferAttribute(door.geometry.attributes.uv.array, 2)
+);
 door.position.y = 2 / 2;
 door.position.z = 4 / 2 + 0.01;
 // door.position.z = 4 / 2 + 0.75 + 0.01;
@@ -199,9 +221,9 @@ window.addEventListener('resize', () => {
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
-camera.position.x = 8;
-camera.position.y = 7;
-camera.position.z = 12;
+camera.position.x = 3;
+camera.position.y = 3;
+camera.position.z = 8;
 scene.add(camera);
 
 // Controls
